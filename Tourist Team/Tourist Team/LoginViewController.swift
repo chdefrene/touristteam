c5bcf4fc2259ea3dcb0e3c279cc3c674bff94826
Katja <katja.maenpaa@aalto.fi>
2017-02-07 15:58:18 +0100

    Add login functionaility

--- Tourist Team/Tourist Team/LoginViewController.swift
+++ Tourist Team/Tourist Team/LoginViewController.swift
@@ -14,6 +14,9 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var Password: UITextField!
     @IBOutlet weak var Loginbtn: UIButton!
     
+    var useraccounts = ["Username": "Password", "User": "pass", "Test": "test"]
+    
+    
     
     
     override func viewDidLoad() {
@@ -39,8 +42,45 @@ class LoginViewController: UIViewController {
     */
     
     @IBAction func loginAction(_ sender: Any) {
-        print ("add functionality here")
+        if ((Username.text == "") || (Password.text == "")) {
+            print("Username or password is empty")
+            emptyCredentials()
+        }
+        if ((Username.text != nil) && (Password.text != nil)) {
+            print ("you can login !")
+            let username = Username.text
+            let password = Password.text
+            
+            if (useraccounts[username!] != nil) {
+                print("Username was found in useraccounts")
+                if (useraccounts[username!] == password ){
+                    print ("access granted !")
+                }else{
+                    print("something wrong with password")
+                    wrongCredentials()
+                }
+            }else{
+                print("username wasn't found")
+                wrongUsername()
+            }
+        }
+    }
     
+    func emptyCredentials() -> Void {
+        let alertmessage = UIAlertController(title: "Login error", message: "Username or password field is empty, please try again.", preferredStyle: .alert)
+        alertmessage.addAction(UIAlertAction(title: "Okay", style: .default))
+        present(alertmessage, animated: true, completion: nil)
+    }
+    
+    func wrongCredentials() -> Void {
+        let alertmessage = UIAlertController(title: "Wrong credentials", message: "Your username and password do not match. Please try again.", preferredStyle: .alert)
+        alertmessage.addAction(UIAlertAction(title: "Okay", style: .default))
+        present(alertmessage, animated: true, completion: nil)
+    }
+    func wrongUsername() -> Void {
+        let alertmessage = UIAlertController(title: "Wrong Username", message: "Your username is typed wrong.", preferredStyle: .alert)
+        alertmessage.addAction(UIAlertAction(title: "Okay", style: .default))
+        present(alertmessage, animated: true, completion: nil)
     }
     
 
